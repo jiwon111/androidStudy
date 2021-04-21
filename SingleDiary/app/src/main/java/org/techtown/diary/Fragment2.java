3d4b32d4df31c9db9bea16a1ae8e5d706e65f7d8
jiwon111 <dreamjiwon@naver.com>
2021-04-21 22:08:22 +0900

    input view

--- SingleDiary/app/src/main/java/org/techtown/diary/Fragment2.java
+++ SingleDiary/app/src/main/java/org/techtown/diary/Fragment2.java
@@ -1,4 +1,95 @@
 package org.techtown.diary;
 
-public class Fragment2 {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+
+import com.github.channguyen.rsv.RangeSliderView;
+
+public class Fragment2 extends Fragment {
+
+    Context context;
+    OnTabItemSelectedListener listener;
+
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        this.context = context;
+
+        if(context instanceof OnTabItemSelectedListener){
+            listener = (OnTabItemSelectedListener) context;
+        }
+    }
+
+    @Override
+    public void onDetach(){
+        super.onDetach();
+
+        if(context != null){
+            context = null;
+            listener = null;
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    //버튼을 눌렀을 때 리스트 화면으로 이동하도록
+    private void initUI(ViewGroup rootView){
+
+        Button saveButton = rootView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                if(listener != null){
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+        Button deleteButton = rootView.findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                if (listener != null){
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+
+        Button closeButton = rootView.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listener != null){
+                    listener.onTabSelected(0);
+                }
+            }
+        });
+
+        RangeSliderView sliderView = rootView.findViewById(R.id.sliderView);
+        sliderView.setOnSlideListener(new RangeSliderView.OnSlideListener() {
+            @Override
+            public void onSlide(int index) {
+                Toast.makeText(context, "moodIndex changed to "+index, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        sliderView.setInitialIndex(2);
+    }
 }
